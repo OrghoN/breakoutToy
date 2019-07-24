ccf559253c229b01c78c9d250881146906dba730
OrghoN <anoronyo@gmail.com>
2019-07-24 00:29:49 -0500

    define basic data structures

--- breakout.py
+++ breakout.py
@@ -1,5 +1,13 @@
 import tkinter
+from collections import namedtuple
 
+#Define Data Structures
+Point = namedtuple('Point', ['x', 'y'])
+Ball = namedtuple('Ball', ['center', 'radius', 'color', 'velocity'])
+Brick = namedtuple('Brick', ['bottomLeft', 'topRight', 'color', 'health'])
+Paddle = namedtuple('Paddle', ['bottomLeft', 'topRight', 'color'])
+
+#Define initial Parameters
 INITIAL_WIDTH = 1600
 INITIAL_HEIGHT = 900
 
